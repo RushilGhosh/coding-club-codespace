5116e5aca5119d3adc6093b1a18407713e0b4451
RushilGhosh <102837364+RushilGhosh@users.noreply.github.com>
2025-03-20 05:35:38 +0000

    The plot thickens
    Added the control information and the first branch
    of user interface for the plot, more work needs to
    be done to link the branches in the plot and
    hopefully declutter the code but that is for later

--- textbasedadventure.py
+++ textbasedadventure.py
@@ -1,1 +1,16 @@
 #text based adventure
+
+print("Press 1 for option [1], 2 for option [2], 3 for option [3], etc...")
+
+print("You are Shayne Hopkins")
+print("You see C. Diddy and John C. Brust kissing! What do you do?")
+choiceOne = input("[1]: join in, [2]: get mad, [3]: walk away")
+
+if choiceOne == "1":
+    print("You kiss agressively until C. Diddy goes 'no you're as big as the boulder'. You start fighting and you trip over a toilet and fall through it. When you come wake up you, C. Diddy, and John C. Brust are trapped in an outhouse with a single window shaped like a moon. You open the door and you see a swamp. All Star by Smash Mouth starts playing.")
+elif choiceOne == "2":
+    print("They get mad back. You start fighting and you trip over a toilet and fall through it. When you come wake up you, C. Diddy, and John C. Brust are trapped in an outhouse with a single window shaped like a moon. You open the door and you see a swamp. All Star by Smash Mouth starts playing.")
+elif choiceOne == "3": 
+    print("You're sad because you get no kisses")
+else:
+    print("Thats not a choice dumb dumb")
